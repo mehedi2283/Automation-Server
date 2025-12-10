f71a744c2b1459b1ec0792a068e63f458cd2c384
mehedi2283 <mehedihasan123456789.mh.mh@gmail.com>
2025-12-10 16:46:58 +0600

    update 28

--- index.js
+++ index.js
@@ -521,6 +521,11 @@ app.post('/api/about', async (req, res) => {
   }
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
